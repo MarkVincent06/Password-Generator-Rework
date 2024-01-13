9f5b4174d651dd3b01b923e690ebf60d50930093
Blueboltz <vincentmariscotescleofe@gmail.com>
2024-01-13 19:25:50 +0800

    finished the primary requirements for generating password

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const characters = ["A","B","C","D","E","F","G","H",
 "6", "7", "8", "9","~","`","!","@","#","$","%",
 "^","&","*","(",")","_","-","+","=","{","[","}",
 "]",",","|",":",";","<",">",".","?","/"];
+
+const generatedPassFieldOne = document.querySelector("#generated-p-field1");
+const generatedPassFieldTwo = document.querySelector("#generated-p-field2");
+const generatePassButton = document.querySelector("#generate-p-btn");
+
+let passLength = 15;
+let generatedPassOne;
+let generatedPassTwo;
+
+generatePassButton.addEventListener("click", () => {
+  generatedPassOne = "";
+  generatedPassTwo = "";
+  for (let i = 0; i < passLength; i++) {
+    generatedPassOne += generateRandomChar();
+    generatedPassTwo += generateRandomChar();
+  }
+
+  generatedPassFieldOne.value = generatedPassOne;
+  generatedPassFieldTwo.value = generatedPassTwo;
+});
+
+function generateRandomChar() {
+  let randomIndex = Math.floor(Math.random() * characters.length);
+  return characters[randomIndex];
+}
